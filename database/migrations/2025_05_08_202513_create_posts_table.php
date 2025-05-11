946286775310b44911dbb11939c58cd269a11d4a
isaque <isaqueclaudino12@gmail.com>
2025-05-11 20:07:02 -0300

    fix(main): add cascade on delete in users table

--- database/migrations/2025_05_08_202513_create_posts_table.php
+++ database/migrations/2025_05_08_202513_create_posts_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
             $table->text('content');
             $table->unsignedBigInteger('user_id');
 
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
             $table->timestamps();
             $table->softDeletes();
         });
